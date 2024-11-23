77f6feca4cbc540c1ad5be7c43a967464e55aba0
Drini23 <122325981+Drini23@users.noreply.github.com>
2024-11-23 22:30:18 +0100

    secrets

--- rent/settings.py
+++ rent/settings.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 import os
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -7,8 +8,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('DJANGO_SECRET_KEY', 'django-insecure-1_&cu(ef6)#@bnnqtc5d0%!1ln8w7415)*8!v#56a&9@!ujb5g')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -95,7 +94,7 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATIC_ROOT = BASE_DIR / 'staticfile'
 
-# Media files (User uploaded files)
+# Media files (User uploaded files)l
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media/'
 
@@ -105,20 +104,16 @@ if not os.path.exists(MEDIA_ROOT):
 # Default primary key field type
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+# Secret key
+SECRET_KEY = config('DJANGO_SECRET_KEY', default='fallback_secret_key')
 
-STRIPE_PUBLIC_KEY = "pk_test_51QEBMgASNj1dXTFJLU85gpWXBousVCOiUbvkckbg6QicHfJRHMfhc7825KkNGJlktFAYP6AI8k1GJxZvcxPVs14O00rU1Q7iYZ"
-STRIPE_SECRET_KEY = "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"
-STRIPE_WEBHOOK = ""
+# Stripe keys
+STRIPE_PUBLIC_KEY = config('STRIPE_PUBLIC_KEY')
+STRIPE_SECRET_KEY = config('STRIPE_SECRET_KEY')
+STRIPE_WEBHOOK = config('STRIPE_WEBHOOK', default='')
+STRIPE_SECRET_KEY_TEST_MODE = config('STRIPE_SECRET_KEY_TEST_MODE')
 
-STRIPE_SECRET_KEY_TEST_MODE = 'sk_test_51QEBMgASNj1dXTFJrYD52zNBUEgTIMvP9O1hUuRz40clVe1GARTCSXFfnt0JiiDReLg0UzxfE3x2sAHPcoRGRe8c00YX3P7rZW'
-
-# Email Settings
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'cacidrini9@gmail.com.com'
-EMAIL_HOST_PASSWORD = 'drini123'
-
-# Default email for contact form
-DEFAULT_CONTACT_EMAIL = 'drinisr@gmail.com'
+# Email settings
+EMAIL_HOST_USER = config('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
+DEFAULT_CONTACT_EMAIL = config('DEFAULT_CONTACT_EMAIL', default='admin@example.com')
